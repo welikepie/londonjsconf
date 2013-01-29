903d1aeb9c724bf2462c25ac90b684da708f05c6
Alexander Michael Roche <alex@amroche.co.uk>
2013-01-29 22:33:00 +0000

    Hashlinks working using the history API as well. Good night.

--- script/history.js
+++ script/history.js
@@ -1,11 +1,9 @@
-
 if(supports_history_api()){
 	console.log("history!");
 }
 else{
-	console.log("Or not...");
+	console.log("No history support :( ");
 }
-
 function supports_history_api() {
   return !!(window.history && history.pushState);
 }
---
+++ script/transfers.js
@@ -0,0 +1,168 @@
+( function() {"use strict";
+		var hashThing = document.URL.substr(document.URL.indexOf('#'), document.URL.length);
+		//#blurb, #mailSubscribe, #speakerSubmit
+		var hashes = ["#blurb", "#mailSubscribe", "#speakerSubmit"];
+		var increment = 0;
+		var active = $('.slideshow .slide.active');
+		var totalSlides = $('.slide');
+		var hammer = new Hammer(document.getElementsByClassName("slideshow")[0]);
+		if (hashThing.indexOf("#") != -1) {
+			changeScreen(hashThing);
+			history.pushState({
+				hash : hashThing
+			}, "", hashThing);
+		}//sort it defaulting to #blurb
+
+		hammer.onswipe = function(ev) {
+			var direction = ev.direction;
+			var active = $('.slideshow .slide.active');
+			var totalSlides = $('.slide');
+			if (direction == "right") {//left moving
+				backOne();
+			}
+			if (direction == "left") {//right moving
+				nextOne();
+			}
+		};
+
+		$(document).keydown(function(e) {
+			if (e.keyCode == 37) {
+				backOne();
+			}
+			if (e.keyCode == 39) {
+				nextOne();
+			}
+		});
+
+		$('#prev-button').on('click', function() {
+			backOne();
+		});
+		$('#next-button').on('click', function() {
+			nextOne();
+		});
+
+		window.addEventListener('popstate', function(event) {
+			//			console.log('popstate fired!');
+			//console.log(event.state);
+
+			if (event.state != null) {
+				console.log(event.state.hash);
+				changeScreen(event.state.hash);
+			}
+
+			//get event state, calculate distance from current to history using hashes and increment,
+			//simulate clicks off of that.
+			//$('#elementid').click();
+
+		});
+		function changeScreen(hashToChange) {
+
+			var index;
+			for (var i = 0; i < hashes.length; i++) {
+				if (hashToChange == hashes[i]) {
+					index = i;
+				}
+			}
+
+			//console.log(index+","+increment);
+			//"next-button" , "prev-button"
+			if (index > increment) {
+				console.log("index");
+				var nums = index - increment;
+				for (var i = 0; i < nums; i++) {
+					console.log("clicking next");
+					var next = active.next();
+					if (next.length) {
+						//increment++;
+						//historySwapper(increment);
+						active.removeClass('active');
+						next.addClass('active');
+						active = next;
+						if (active[0] != totalSlides[0]) {
+							document.getElementById("prev-button").className = "";
+						}
+						if (active[0] == totalSlides[totalSlides.length - 1]) {
+							document.getElementById("next-button").className = "hidden";
+						}
+					}
+
+				}
+				increment = index;
+			} else if (increment > index) {
+				console.log("increment");
+				var nums = increment - index;
+				for (var i = 0; i < nums; i++) {
+					console.log("clicking prev");
+					var prev = active.prev();
+					if (prev.length) {
+						//increment--;
+						//historySwapper(increment);
+						active.removeClass('active');
+						prev.addClass('active');
+						active = prev;
+						if (active[0] == totalSlides[0]) {
+							document.getElementById("prev-button").className = "hidden";
+						}
+						if (active[0] != totalSlides[totalSlides.length - 1]) {
+							document.getElementById("next-button").className = "";
+						}
+					}
+				}
+				increment = index;
+			}
+
+		}
+
+		function nextOne() {
+			var next = active.next();
+			if (next.length) {
+				increment++;
+				historySwapper(increment);
+				active.removeClass('active');
+				next.addClass('active');
+				active = next;
+				if (active[0] != totalSlides[0]) {
+					document.getElementById("prev-button").className = "";
+				}
+				if (active[0] == totalSlides[totalSlides.length - 1]) {
+					document.getElementById("next-button").className = "hidden";
+				}
+			}
+
+		}
+
+		function backOne() {
+			var prev = active.prev();
+			if (prev.length) {
+				increment--;
+				historySwapper(increment);
+				active.removeClass('active');
+				prev.addClass('active');
+				active = prev;
+				if (active[0] == totalSlides[0]) {
+					document.getElementById("prev-button").className = "hidden";
+				}
+				if (active[0] != totalSlides[totalSlides.length - 1]) {
+					document.getElementById("next-button").className = "";
+				}
+
+			}
+		}
+
+		function historySwapper(num) {
+			console.log(num);
+			console.log(hashes[num]);
+			//var hashlink = '"'+hashes[num].toString()+'"';
+			var stateObj = {
+				hash : hashes[num]
+			};
+			history.pushState(stateObj, "", hashes[num]);
+		}
+
+		if (hashThing.indexOf("#") == -1) {
+			history.pushState({
+				hash : "#blurb"
+			}, "", hashes[0]);
+		}
+
+	}());
